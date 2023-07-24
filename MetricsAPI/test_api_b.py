a8e5fc75c161fbc94e0e239ed71f1e5f34fa57c0
Yebeltal Abebe <abebe3@un.org>
2023-07-24 10:54:15 +0300

    Update test_api_b.py

--- MetricsAPI/test_api_b.py
+++ MetricsAPI/test_api_b.py
@@ -4,7 +4,6 @@ from app import get_average_monthly_value_metrics_a
 class TestAPIB(unittest.TestCase):
 
     def test_calculate_daily_national_estimate_metrics_b(self):
-        result = get_average_monthly_value_metrics_a("col","2022-06-01","2023-07-01")
-        expected_output = null
+        result = get_average_monthly_value_metrics_a("col","2022-06-01","2023-07-01")        
 
         self.assertEqual(result, None)
